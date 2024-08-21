8c7d9d0a648c93f6c2485b0268fb64fb8d4ceb09
gloria8395 <gloria0906700268@gmail.com>
2024-08-21 12:19:17 +0000

    modify

--- lesson8/lesson8_2.py
+++ lesson8/lesson8_2.py
@@ -1,4 +1,4 @@
-import widget
+from widget import tools  # from 後面可放 package(資料夾) 或 module
 
 #主程式
 while True:
@@ -7,7 +7,7 @@ while True:
         height=float(input('請輸入身高(cm):'))
         weight=float(input('請輸入體重(kg):'))
         bmi=weight/((height*0.01)**2)
-        grade=widget.tools.get_status_message(bmi)
+        grade=tools.get_status_message(bmi)
         print(f"{name} 的 BMI 為 {bmi:.2f},為{grade}")
     except ValueError:
         print("格式錯誤")
